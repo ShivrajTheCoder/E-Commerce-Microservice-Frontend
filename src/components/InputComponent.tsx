55d9db13f38da6198d9b1a7b64cd6e059cd4bed0
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-06-12 15:55:41 +0530

    loginscreen-css-added

--- src/components/InputComponent.tsx
+++ src/components/InputComponent.tsx
@@ -14,9 +14,9 @@ export default function InputComponent(props:InputProps) {
   // console.log(props)
   const {id,type,name,label,placeholder}=props;
   return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input type={type} name={name} id={id} value={props.value} placeholder={placeholder} onChange={props.onChange} />    
+    <div className='w-full h-fit py-3 flex flex-col'>
+      <label htmlFor={id} className='font-bold text-lg'>{label}</label>
+      <input className='bg-white h-10 px-3 border-l-4 border-blue-500' type={type} name={name} id={id} value={props.value} placeholder={placeholder} onChange={props.onChange} />    
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,50 @@
+import Link from "next/link";
 import InputComponent from "../components/InputComponent"
 import { useState } from 'react'
 export default function Home() {
-  const [inputValues,setInputValues]=useState({
-    email:"",
-    password:"",
+  const [inputValues, setInputValues] = useState({
+    email: "",
+    password: "",
   })
-  const {email,password}=inputValues;
-  const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
-    const {name,value}=e.target;
-    setInputValues((prev)=>({
+  const { email, password } = inputValues;
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInputValues((prev) => ({
       ...prev,
-      [name]:value,
+      [name]: value,
     }))
   }
-  const submitHandler=(e:React.SyntheticEvent)=>{
+  const submitHandler = (e: React.SyntheticEvent) => {
     e.preventDefault();
     console.log(inputValues);
   }
   return (
-    <main>
-      <form onSubmit={submitHandler}>
-        <InputComponent id="email"
-              name="email"
-              type="email"
-              label="Email"
-              value={email}
-              onChange={handleChange}
-              placeholder="Enter Email" />
-        <InputComponent id="password"
-              name="password"
-              type="password"
-              label="Password"
-              value={password}
-              onChange={handleChange}
-              placeholder="Enter Password" />
-          <button>Sign In</button>
-      </form>
+    <main className="mx-10 mb-10 flex flex-col justify-center items-center ">
+      <section className="flex my-10 w-[60%] shadow-lg">
+        <div className="w-[70%]">
+          <img className="h-[500px] w-full rounded-l-2xl" src="https://res.cloudinary.com/dushmacr8/image/upload/v1686564281/Micro-Ecommerce/homescreen/login_qkhhpd.jpg" alt="girl with headphone" />
+        </div>
+        
+        <form onSubmit={submitHandler} className="bg-[#f6f6f6] rounded-r-2xl w-full px-10 flex flex-col justify-center items-center">
+          <h1 className="font-bold text-3xl">Login</h1>
+          <InputComponent id="email"
+            name="email"
+            type="email"
+            label="Email"
+            value={email}
+            onChange={handleChange}
+            placeholder="Enter Email" />
+          <InputComponent id="password"
+            name="password"
+            type="password"
+            label="Password"
+            value={password}
+            onChange={handleChange}
+            placeholder="Enter Password" />
+          <button className='font-bold mt-10 text-xl bg-black text-white hover:bg-white hover:text-black px-5 py-3 rounded-md'>Sign In</button>
+        <div className="my-2 font-semibold text-md">Not a user? <Link className="text-blue-500" href={"/home"}>Signup</Link></div>
+        </form>
+      </section>
     </main>
   )
 }
