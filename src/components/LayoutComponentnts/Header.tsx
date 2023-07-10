1af12372cccfa213cce6b0b3e154e03ee9e0de8f
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2023-07-11 01:58:14 +0530

    auction-order-added

--- src/components/LayoutComponentnts/Header.tsx
+++ src/components/LayoutComponentnts/Header.tsx
@@ -1,15 +1,20 @@
 import { RootState } from '@/store/reducers';
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { BsFillPersonFill } from 'react-icons/bs';
 import { useSelector } from 'react-redux';
 export default function Header() {
+    const [isDropdownOpen, setIsDropdownOpen] = useState<Boolean>(false);
     const user = useSelector((state: RootState) => state.user);
     const { items } = useSelector((state: RootState) => state.cart);
     // console.log(items.length, "front the header")
     const isAdmin = user.isAdmin;
+    const isLoggedIn=user.isLoggedin;
     // console.log(user, "user sate");
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
     return (
         <nav className='flex mx-10 my-2'>
             <Link className=' font-extrabold font-mono text-2xl' href="/home">Online.Shopping</Link>
@@ -31,12 +36,32 @@ export default function Header() {
                 <Link href={"/user/myorders"} className='mx-5 text-lg font-bold text-white  bg-[#A3A4A1] rounded-3xl px-5 py-1'>My Orders</Link>
             </div>
             <div className='ml-auto mx-2 flex'>
-                <Link href="/" className='mx-3 flex justify-center '><BsFillPersonFill size={30} style={{ color: "#03045e" }} /><span className='text-xl  font-semibold text-[#03045e]'>Account</span></Link>
                 <Link href="/user/cart" className='flex'><AiOutlineShoppingCart size={30} style={{ color: "#03045e" }} />{
                     items.length > 0 &&
                     <span className='bg-red-500 text-white  rounded-full h-fit w-fit px-2'>{items.length}</span>
                 }
                 </Link>
+                {
+                    !isLoggedIn && 
+                    <Link href="/" className='mx-3 flex justify-center'>
+                        <BsFillPersonFill size={30} className="text-[#03045e]" />
+                        <span className='text-xl font-semibold text-[#03045e]'>Account</span>
+                    </Link>
+                }
+                { isLoggedIn && <div className="relative">
+                    <button onClick={toggleDropdown} className='mx-3 flex justify-center'>
+                        <BsFillPersonFill size={30} className="text-[#03045e]" />
+                        <span className='text-xl font-semibold text-[#03045e]'>Account</span>
+                    </button>
+                    {isDropdownOpen && (
+                        <div className="absolute right-0 mt-2 w-40 bg-white rounded-md shadow-lg z-10">
+                            {/* Add your dropdown links here */}
+                            <Link href="/user/myorders" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">My Orders</Link>
+                            <Link href="/user/myaucorders" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Auction Orders</Link>
+                            
+                        </div>
+                    )}
+                </div>}
             </div>
         </nav>
     )
---
+++ src/components/OrderComponents/AuctionOrderCard.tsx
@@ -0,0 +1,77 @@
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react'
+import RazorpayButton from '../PaymentComponent/RazorpayButton';
+interface IAucOrder {
+    createdAt: string;
+    item: string;
+    payment: boolean;
+    price: number;
+    userId: string;
+    _id: string;
+    razorpayOrder: string;
+}
+interface OrderProps {
+    order: IAucOrder;
+}
+interface IProduct {
+    name: string;
+    description: string;
+    startingBid: number;
+    minBidInc: number;
+    lastBid: number;
+    image_url: string;
+    date: string;
+    time: string;
+    lastBidUser: string;
+    active: boolean;
+    end: string;
+    _id: string;
+
+}
+export default function AuctionOrderCard({ order }: OrderProps) {
+    console.log(order);
+    const [item, setItem] = useState<IProduct>();
+    const [rzOrder, rzSetOrder] = useState<any>();
+    useEffect(() => {
+        console.log("heere is your data")
+        if (order.item[0]) {// remove this condition
+            setItem(JSON.parse(order.item[0]));
+
+        }
+        // console.log(order.item[0]);
+        rzSetOrder(order.razorpayOrder);
+        // console.log(order.razorpayOrder);
+    }, [order]);
+    return (
+        <section className='m-5'>
+            {
+                (item && rzOrder) && <main className='m-2 p-5 rounded-md '>
+                    <h1 className='py-2 text-3xl font-bold'>Order Id: OnS{order._id}</h1>
+                    <div className='flex text-lg my-5'>
+                        <div>Ordered On {`${order.createdAt.toString()}`}</div>
+                        <div className='border-l-2 bg-blue-500 w-1 h-8 mx-3'></div>
+                        <div>Estimated Delivery {`Delivery date`}</div>
+                        <div className='border-l-2 bg-blue-500 w-1 h-8 mx-3'></div>
+                        <div className='font-bold'>₹ {order.price}</div>
+                    </div>
+                    <div className='grid grid-cols-3 gap-4'>
+                        <img className='col-span-1 rounded-md' src={item.image_url} alt={item.name} />
+                        <div className='col-span-2'>
+                            <h2 className='font-bold text-3xl'>{item.name}</h2>
+                            <div className='col-span-5 font-semibold text-lg'><p>{item.description}</p></div>
+                            <div className='font-bold text-xl'>₹ {order.price}</div>
+                            <div>
+                                <h1 className="font-bold text-xl">
+                                    Payment Status <span className="font-semibold text-lg">{order.payment ? 'Paid' : 'Not Paid'}</span>
+                                </h1>
+                                {!order.payment && rzOrder && (
+                                    <RazorpayButton totalPrice={order.price} order_id={rzOrder.id} or_id={order._id} />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            }
+        </section>
+    )
+}
